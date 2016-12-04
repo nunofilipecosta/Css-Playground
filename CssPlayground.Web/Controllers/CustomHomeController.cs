7f0e67ef10f8fb7a11847b49f23978be6b9357b4
Nuno Costa <nuno.costa@dominos.co.uk>
2016-12-04 21:29:17 +0000

    Add project files.

---
+++ CssPlayground.Web/Controllers/CustomHomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CssPlayground.Web.Controllers
+{
+    public class CustomHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
